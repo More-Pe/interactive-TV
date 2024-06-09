ee9b61d0178fabba5638674fb501b3a6ea30d062
More-Pe <peraltamorena.va@gmail.com>
2024-06-09 13:11:28 +0200

    Channels and info added and working well

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let change = false;
+let infoChange = false;
+
+let tvScreen = document.getElementById("screen");
+let onOffButton = document.getElementById("on-off-button");
+let numberButton = document.getElementsByClassName("channel");
+let infoButton = document.getElementById("info-button");
+let tvInfo = document.getElementById("tv-info");
+let chanelName = document.getElementById("chanel-name");
+let dateTime = document.getElementById("date");
+
+let arrayChanels = Array.from(numberButton);
+
+let now = new Date();
+let hour = String(now.getHours()).padStart(2, "0");
+let minute = String(now.getMinutes()).padStart(2, "0");
+let formattedTime = `${hour}:${minute}`;
+
+let year = now.getFullYear();
+let month = String(now.getMonth() + 1).padStart(2, "0");
+let day = String(now.getDate()).padStart(2, "0");
+let formattedDate = `${day}-${month}-${year}`;
+
+for (let i = 0; i < arrayChanels.length; i++) {
+  arrayChanels[i].addEventListener("click", () => {
+    if (change) {
+      tvScreen.classList.replace(tvScreen.classList[0], `ch${i + 1}`);
+      chanelName.innerHTML = `CH ${i + 1}`;
+    }
+  });
+}
+
+dateTime.innerHTML = formattedDate + "<br>" + formattedTime;
+
+onOffButton.addEventListener("click", () => {
+  tvScreen.classList.toggle("on");
+  change = !change;
+  if (!change) {
+    tvScreen.classList.replace(tvScreen.classList[0], "off");
+    infoChange = false;
+    tvInfo.classList.remove("info-on");
+    chanelName.innerHTML = "";
+  } else {
+    null;
+  }
+});
+
+infoButton.addEventListener("click", () => {
+  if (change) {
+    tvInfo.classList.toggle("info-on");
+    infoChange = !infoChange;
+  }
+});
